b39f55909c19254e0519b9278b8a07cc8b819a7f
r1no933 <dbaskakov93@yandex.ru>
2025-09-25 18:44:43 +0300

    init project

---
+++ event-notification-service/src/main/java/dev/baskakov/eventnotificationservice/EventNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.baskakov.eventnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ event-notification-service/src/test/java/dev/baskakov/eventnotificationservice/EventNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.baskakov.eventnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
