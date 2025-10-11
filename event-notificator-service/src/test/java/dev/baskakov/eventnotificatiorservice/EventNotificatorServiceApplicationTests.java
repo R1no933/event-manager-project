c68178a79a1b46920b7bbc1f8137ee74c4b82525
r1no933 <dbaskakov93@yandex.ru>
2025-10-11 20:52:44 +0300

    исправлены pom файлы для полноценной работы мультимодульного проекта

---
+++ event-notificator-service/src/test/java/dev/baskakov/eventnotificatiorservice/EventNotificatorServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.baskakov.eventnotificatiorservice;
+
+import dev.baskakov.eventnotificatorservice.EventNotificatorServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EventNotificatorServiceApplication.class)
+class EventNotificatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
